b154355a3606fafb02879dac93a922d7c8968936
Petyo Petrov <ppe@zitcom.dk>
2020-08-22 21:01:28 +0300

    Updates

---
+++ precache-manifest.edbcffb59be19987ff667bb78adce545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c81fac0fbc54a9c896e35ebe0b02647",
+    "url": "/calendar.github.io/index.html"
+  },
+  {
+    "revision": "d3314a26a7929c5d11fc",
+    "url": "/calendar.github.io/static/css/main.b4ec6aa4.chunk.css"
+  },
+  {
+    "revision": "889097ccdf384d03aa0a",
+    "url": "/calendar.github.io/static/js/2.2b411ca5.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/calendar.github.io/static/js/2.2b411ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3314a26a7929c5d11fc",
+    "url": "/calendar.github.io/static/js/main.a57ccc1d.chunk.js"
+  },
+  {
+    "revision": "23ee8d3f6f39cefd4d7d",
+    "url": "/calendar.github.io/static/js/runtime-main.4def58f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calendar.github.io/precache-manifest.a93ed81f8947e81c71a585bcd80cd660.js"
+  "/calendar.github.io/precache-manifest.edbcffb59be19987ff667bb78adce545.js"
 );
 
 self.addEventListener('message', (event) => {
