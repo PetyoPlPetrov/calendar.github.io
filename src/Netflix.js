3fa702f37076346460c62687934875f439f7e2ba
Petyo Petrov <ppe@zitcom.dk>
2020-09-09 23:59:21 +0300

    loading text

--- src/Netflix.js
+++ src/Netflix.js
@@ -8,29 +8,37 @@ const Netflix = () => {
     setTimeout(() => setVisible(true), 1000);
   }, []);
   return (
-    <div
-      className="container"
-      style={{ ...(!visible ? { display: "none" } : {}) }}
-    >
-      <a href="#animals" className="item">
-        <img src="https://placeimg.com/640/480/animals" alt="Animals" />
-      </a>
-      <a href="#architecture" className="item">
-        <img
-          src="https://placeimg.com/640/480/architecture"
-          alt="Architecture"
-        />
-      </a>
-      <a href="#nature" className="item">
-        <img src="https://placeimg.com/640/480/nature" alt="Nature" />
-      </a>
-      <a href="#people" className="item">
-        <img src="https://placeimg.com/640/480/people" alt="People" />
-      </a>
-      <a href="#tech" className="item">
-        <img src="https://placeimg.com/640/480/tech" alt="Tech" />
-      </a>
-    </div>
+    <>
+      <div
+        className="container flex centered"
+        style={{ ...(visible ? { display: "none" } : {}) }}
+      >
+        Loading...
+      </div>
+      <div
+        className="container"
+        style={{ ...(!visible ? { display: "none" } : {}) }}
+      >
+        <a href="#animals" className="item">
+          <img src="https://placeimg.com/640/480/animals" alt="Animals" />
+        </a>
+        <a href="#architecture" className="item">
+          <img
+            src="https://placeimg.com/640/480/architecture"
+            alt="Architecture"
+          />
+        </a>
+        <a href="#nature" className="item">
+          <img src="https://placeimg.com/640/480/nature" alt="Nature" />
+        </a>
+        <a href="#people" className="item">
+          <img src="https://placeimg.com/640/480/people" alt="People" />
+        </a>
+        <a href="#tech" className="item">
+          <img src="https://placeimg.com/640/480/tech" alt="Tech" />
+        </a>
+      </div>
+    </>
   );
 };
 export default Netflix;
