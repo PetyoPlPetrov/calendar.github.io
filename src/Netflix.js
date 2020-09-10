8f5e4e6672b4db89e63a76c026f58a1cc6320338
Petyo Petrov <ppe@zitcom.dk>
2020-09-10 11:03:12 +0300

    add transition

--- src/Netflix.js
+++ src/Netflix.js
@@ -7,38 +7,31 @@ const Netflix = () => {
   useEffect(() => {
     setTimeout(() => setVisible(true), 1000);
   }, []);
+
   return (
-    <>
-      <div
-        className="container flex centered"
-        style={{ ...(visible ? { display: "none" } : {}) }}
-      >
-        Loading...
-      </div>
-      <div
-        className="container"
-        style={{ ...(!visible ? { display: "none" } : {}) }}
-      >
-        <a href="#animals" className="item">
-          <img src="https://placeimg.com/640/480/animals" alt="Animals" />
-        </a>
-        <a href="#architecture" className="item">
-          <img
-            src="https://placeimg.com/640/480/architecture"
-            alt="Architecture"
-          />
-        </a>
-        <a href="#nature" className="item">
-          <img src="https://placeimg.com/640/480/nature" alt="Nature" />
-        </a>
-        <a href="#people" className="item">
-          <img src="https://placeimg.com/640/480/people" alt="People" />
-        </a>
-        <a href="#tech" className="item">
-          <img src="https://placeimg.com/640/480/tech" alt="Tech" />
-        </a>
-      </div>
-    </>
+    <div
+      style={{ ...(!visible ? { maxHeight: "13.5rem" } : {}) }}
+      className={`container ${!visible ? "hidden" : "shown"}`}
+    >
+      <a href="#animals" className="item">
+        <img src="https://placeimg.com/640/480/animals" alt="Animals" />
+      </a>
+      <a href="#architecture" className="item">
+        <img
+          src="https://placeimg.com/640/480/architecture"
+          alt="Architecture"
+        />
+      </a>
+      <a href="#nature" className="item">
+        <img src="https://placeimg.com/640/480/nature" alt="Nature" />
+      </a>
+      <a href="#people" className="item">
+        <img src="https://placeimg.com/640/480/people" alt="People" />
+      </a>
+      <a href="#tech" className="item">
+        <img src="https://placeimg.com/640/480/tech" alt="Tech" />
+      </a>
+    </div>
   );
 };
 export default Netflix;
